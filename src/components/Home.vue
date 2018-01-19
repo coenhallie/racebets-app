e3fc35d7d3122e5b07306af0b2c2abc853fd23a6
coenhallie <me@coenhallie.com>
2018-01-19 15:44:24 +0100

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+      <RaceFilter></RaceFilter>
+      </div>
+      <div class="column">
+        Second column
+      </div>
+      <div class="column">
+        Third column
+      </div>
+      <div class="column">
+        <NextRace></NextRace>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NextRace from './NextRace'
+import RaceFilter from './RaceFilter'
+
+export default {
+  name: 'Home',
+  components: {
+    RaceFilter,
+    NextRace
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/RaceFilters.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="block">
+      <b-checkbox v-model="RaceFilters"
+          native-value="Silver">
+          Silver
+      </b-checkbox>
+      <b-checkbox v-model="RaceFilters"
+          native-value="Flint">
+          Flint
+      </b-checkbox>
+      <b-checkbox v-model="RaceFilters"
+          native-value="Vane">
+          Vane
+      </b-checkbox>
+      <b-checkbox v-model="RaceFilters"
+          native-value="Billy">
+          Billy
+      </b-checkbox>
+    </div>
+    {{RaceFilters}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RaceFilters',
+  data () {
+    return {
+      filter: []
+    }
+  },
+  methods: {
+    RaceFilters() {
+      this.filter
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
